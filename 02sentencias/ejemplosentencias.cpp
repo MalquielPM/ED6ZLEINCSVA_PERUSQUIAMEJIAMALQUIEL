9fa7edd0e5af3552b05ecccc6b97d9ad8543e960
malquiel <ma.perusquia.m@edu.utc.mx>
2022-05-25 12:39:20 -0500

    ejemplosentenciadowhhile

---
+++ 02sentencias/ejemplosentencias.cpp
@@ -0,0 +1,15 @@
+#incluide <iostream>
+
+using namespace std;
+
+int main(){
+	  //uso de sentencias de control
+	  //do while
+	  in 1=1;
+	  do{
+	  	//vamos a crear un programa que imprima hasta el 10
+	  	cout<<i<<endl;
+	  	i++;
+	  	while(i<11);
+	  }
+}
