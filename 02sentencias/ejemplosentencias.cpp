2363cd2de91399145d6ef215e896f895950a0de8
malquiel <ma.perusquia.m@edu.utc.mx>
2022-05-25 13:35:16 -0500

    ejemplo while

--- 02sentencias/ejemplosentencias.cpp
+++ 02sentencias/ejemplosentencias.cpp
@@ -13,10 +13,38 @@ int main(){
 	  	while(i<11);
 	  	*/
 	  	//while
-	  	int cont =1;
+	  	/*int cont =1;
 	  	while(cont <=10){
 	  		cout<<cont<<endl;
 	  		cont++;
 		  }
+		  cout<<"se acabo"*/
+		  //for
+		  /*for(int i = 1; i<=10; i++)
+		      cout<<i<<endl;
+		      */
+		      int seleccion;
+		      
+		      do{
+		      	cout<<"\n menu 1, 2, 3, otra opcion salir \n";
+		      	cout<<"\n1,- opcion 1 \n";
+		      	cout<<"\n2,- opcion 2 \n";
+		      	cout<<"\n3,- opcion 3 \n";
+		      	cout<<"\n salir \n";
+		      	cout<<"\n ingrese la opcion \n";
+		      	case 1;
+		      	cout<<"opcion 1";
+		      	break;
+		      	case 2;
+		      	cout<<"opcion 2";
+		      	break;
+		      	case 3;
+		      	cout<<"opcion 3";
+		      	break;
+		      	default;
+		      	cout<<"ayos";
+		      	break;
+		      	while((seleccion > 0 ))
+			  }
 	  }
 }
