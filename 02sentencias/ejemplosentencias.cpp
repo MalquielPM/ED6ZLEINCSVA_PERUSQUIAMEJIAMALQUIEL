8ce8da3d79e8368194d250fc98f49cd620aa0b57
malquiel <ma.perusquia.m@edu.utc.mx>
2022-05-25 13:11:41 -0500

    ejemplowhile

--- 02sentencias/ejemplosentencias.cpp
+++ 02sentencias/ejemplosentencias.cpp
@@ -5,11 +5,18 @@ using namespace std;
 int main(){
 	  //uso de sentencias de control
 	  //do while
-	  in 1=1;
+	  /*in 1=1;
 	  do{
 	  	//vamos a crear un programa que imprima hasta el 10
 	  	cout<<i<<endl;
 	  	i++;
 	  	while(i<11);
+	  	*/
+	  	//while
+	  	int cont =1;
+	  	while(cont <=10){
+	  		cout<<cont<<endl;
+	  		cont++;
+		  }
 	  }
 }
